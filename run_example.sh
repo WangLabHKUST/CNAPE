17a8cec3bf8864b757db83911f773f42ba50eac3
kevin Mu <kevinmu@dy036-176.ust.hk>
2018-09-28 14:08:16 +0800

    add test

--- run_example.sh
+++ run_example.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
+#the cnape.R takes two arguments:
+#the first one is your expression matrix
+#the format is examplified in the example/example.geneExpression.RPKM.txt
+#the RPKM values should be prepared following the TCGA pipeline
+
+#the second argument is the prefix of the output.
+#Two files will be written to the output:
+#${prefix}.chromosome_level.cna.txt       ${prefix}.arm_level.cna.txt
 Rscript cnape.R example/example.geneExpression.RPKM.txt example/example
